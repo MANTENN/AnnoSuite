cbfd260e857911e9e5921e4a7000d5ba263bebf7
Nazar Maksymchuk <nzrbeats@gmail.com>
2018-06-09 02:03:32 -0700

    Changes: Boilerplate

--- prototype/js/annoweb.js
+++ prototype/js/annoweb.js
@@ -1,100 +1,99 @@
-(function() { 
+(function() {
     var annoweb = function () {
-        this.init
-    };
-    annoweb.init = function () {
-        var self = this;
-        this.setDefaults = function (commentColor, highlightColor) {
-            if(commentColor && highlightColor && commentColor.length == 6 && highlightColor.length == 6) {
-                this.config.state.comments.color = commentColor;
-                this.config.state.highlights.color = highlightColor;
+        //Function Declarations
+        this.keyboardTextSelection = function(e) {
+            return e.shiftKey && e.keyCode >= 37 && e.keyCode <= 40
+        }
+        this.mouseDown = function (e) {
+            if(this.keyboardTextSelection(e)) {
+                console.log("Text selected!");
+            }
+            console.log(e);
+        }
+        this.keyDown = function (e) {
+            console.log(e);
+        }
+        this.init = function () {
+            console.log("Initializing Annoweb...")
+            window.addEventListener("mousedown", annoweb.mouseDown)
+            window.addEventListener("keydown", annoweb.mouseDown)
+            this.state = {
+                'textSelected': false,
+                'comments': {
+                    'visible': false,
+                },
+                'highlights': {
+                    'visible': false,
+                }
+            };
+            this.config = {
+                'comments': {
+                    'colors': {
+                        //@TODO: store hex values
+                        'default': {
+                            'yellow': 'fdffb3',
+                            'purple': 'd5b3ff'
+                        },
+                    }
+                },
+                'highlights': {
+                    'colors': {
+                        'default': {
+                            'yellow': 'fdffb3',
+                            'purple': 'd5b3ff'
+                        }
+                    }
+                }
+            };
+        }
+        //@TODO: Wrap Individual Elements or Group Wrap
+        //@TODO: Implement saving method
+        var elementSelector = function () {
+            return arguments.map(function(element) {
+                return document.getElementById('element');
+            });
+        }
+        this.addHighlight = function (element) {
+            //@TODO: Append to the dom, add it to indexed DB
+        }
+        this.removeHighlight = function (element, id) {
+            //@TODO: Remove from the dom, and remove it from indexed DB
+        }
+        this.addComment = function (comment) {
+            //@TODO: Append to the dom, and add it to indexedb
+        }
+        this.removeComment = function (comment, id) {
+            //@TODO: Remove from the dom, and remove it from indexedb
+        }
+        this.addNotes = function (note) {
+            this.storeData({type: 'note', data: note})
+        }
+        this.removeNote = function (element, noteId) {
+
+        }
+        this.storeData = function (item) {
+            var itemData = JSON.parse(local.storage.getItem(item.type))
+            if(!itemData && itemData.construc) {
+                itemData.push(item.data);
                 return true;
             }
             return false;
         }
-        this.config = {
-            'state': {
-                'comments': {
-                    'visible': false,
-                    'color': '',
-                    'eat': function () {
-                        console.log(this);
-                    }
-                },
-                'highlights': {
-                    'visible': false,
-                    'color': ''
-                }
-            },
-            'comments': {
-                'colors': {
-                    //@TODO: store hex values
-                    'default': {
-                        'yellow': 'fdffb3',
-                        'purple': 'd5b3ff'
-                    },
-                }
-            },
-            'highlights': {
-                'colors': {
-                    'default': {
-                        'yellow': 'fdffb3',
-                        'purple': 'd5b3ff'
-                    }
-                }
-        }
-    };
-    var state = this.setDefaults(this.config.comments.color, this.config.highlights.color);
-    console.log(state);
-    if(!state) {
-        console.log("Initial state failed initialization process.")
-    }
-    console.log(this.config);
-
-        //@TODO: Initialize Default Configuration
-        //@TODO: Add Event Listener on Page Load
-    }
-    //@TODO: Wrap Individual Elements or Group Wrap
-    //@TODO: Implement saving method 
-    var elementSelector = function () {
-        return arguments.map(function(element) {
-            return document.getElementById('element');
-        });
-    }
-    annoweb.addHighlight = function (element) {
-
-    }
-    annoweb.removeHighlight = function (element, id) {
-
-    }
-    annoweb.addComment = function (comment) {
-
-    }
-    annoweb.removeComment = function (comment, id) {
-
-    }
-    annoweb.addNotes = function (note) {
-        this.storeData({type: 'note', data: note})
-    }
-    annoweb.removeNote = function (element, noteId) {
-
-    }
-    annoweb.storeData = function (item) {
-        var itemData = JSON.parse(local.storage.getItem(item.type))
-        if(!itemData && itemData.construc) {
-            itemData.push(item.data);
+        //based on the precondition that data is sorted
+        this.removeData = function(type, id) {
+            var itemData = JSON.parse(localStorage.getItem(type))
+            if(!itemData) {
+                return false
+            }
+            itemData[id] = false;
             return true;
         }
-        return false;
-    }
-    //based on the precondition that data is sorted
-    annoweb.removeData = function(type, id) {
-        var itemData = JSON.parse(localStorage.getItem(type))
-        if(!itemData) {
-            return false
-        }
-        itemData[id] = false;
-        return true;
-    }
-     annoweb.init();
+        console.log(this)
+        //Coupling Area
+        this.init.bind(this);
+        this.mouseDown.bind(this);
+        this.keyDown.bind(this);
+    };
+    annoweb = new annoweb();
+    annoweb.init()
 })();
